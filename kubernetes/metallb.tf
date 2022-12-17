dfe39c5cc4aa38b1740b0f843e250bb7ee114f20
KangDroid <hyunwoo286@gmail.com>
2022-12-18 01:51:53 +0900

    [Fix] Use Official Helm Chart, instead of Binami Charts
    Since Binarmi Charts does not support AARCH64 Architecture

--- kubernetes/metallb.tf
+++ kubernetes/metallb.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "metallb" {
   name       = "metallb"
-  repository = "https://charts.bitnami.com/bitnami"
+  repository = "https://metallb.github.io/metallb"
   chart      = "metallb"
   namespace  = kubernetes_namespace_v1.infrastructure.metadata.0.name
 }
