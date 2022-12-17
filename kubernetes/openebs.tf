623aae873178cf045f8ccd1e4c5f16339dfc67a8
KangDroid <hyunwoo286@gmail.com>
2022-12-18 02:06:58 +0900

    [Feature] Infrastructure: Provision OpenEBS

---
+++ kubernetes/openebs.tf
@@ -0,0 +1,7 @@
+resource "helm_release" "openebs" {
+  name       = "openebs"
+  repository = "https://openebs.github.io/charts"
+  chart      = "openebs"
+  namespace  = kubernetes_namespace_v1.infrastructure.metadata.0.name
+}
+
