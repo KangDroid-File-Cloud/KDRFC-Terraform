1624482550ed534dcc7c42bfda971ccddf25fc73
KangDroid <hyunwoo286@gmail.com>
2022-12-18 23:24:57 +0900

    [Fix] Azure SQL: Use path.module variable

--- applications/modules/infrastructure/azuresql.tf
+++ applications/modules/infrastructure/azuresql.tf
@@ -1,5 +1,5 @@
 resource "helm_release" "sqlserver" {
   name      = "sqlserver"
-  chart     = "./chart"
+  chart     = "${path.module}/chart"
   namespace = kubernetes_namespace_v1.name.metadata.0.name
 }
