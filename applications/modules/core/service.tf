f1c603ee92927392c35cafd85df9c7e8bdfd3c2a
KangDroid <hyunwoo286@gmail.com>
2022-12-19 18:14:40 +0900

    [Feature] Application Core/Service: Expose Service to public

---
+++ applications/modules/core/service.tf
@@ -0,0 +1,48 @@
+resource "kubernetes_service_v1" "kdrfc_core" {
+  metadata {
+    name      = "kdrfc-core"
+    namespace = kubernetes_namespace_v1.application_core.metadata.0.name
+  }
+
+  spec {
+    type = "ClusterIP"
+
+    port {
+      port        = 80
+      target_port = 80
+      protocol    = "TCP"
+    }
+
+    selector = {
+      "app" = "kdrfc-core"
+    }
+  }
+}
+
+resource "kubernetes_ingress_v1" "kdrfc_core" {
+  metadata {
+    name      = "kdrfc-core"
+    namespace = kubernetes_namespace_v1.application_core.metadata.0.name
+    annotations = {
+      "kubernetes.io/ingress.class" = "traefik"
+    }
+  }
+
+  spec {
+    rule {
+      http {
+        path {
+          path = "/"
+          backend {
+            service {
+              name = kubernetes_service_v1.kdrfc_core.metadata.0.name
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
