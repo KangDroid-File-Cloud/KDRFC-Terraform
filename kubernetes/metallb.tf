b20549e7ce9e5aab98cef063716da2abf9d90bc0
KangDroid <hyunwoo286@gmail.com>
2022-12-19 10:29:22 +0900

    [Chore] Kubernetes Infrastructure: Remove MetalLB Since K3s cluster does have default LB Implementation

--- kubernetes/metallb.tf
+++
@@ -1,6 +0,0 @@
-resource "helm_release" "metallb" {
-  name       = "metallb"
-  repository = "https://metallb.github.io/metallb"
-  chart      = "metallb"
-  namespace  = kubernetes_namespace_v1.infrastructure.metadata.0.name
-}
