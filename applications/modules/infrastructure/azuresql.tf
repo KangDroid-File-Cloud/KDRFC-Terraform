5eec83f7e1c22d16174766c6686f1b26d932e9b4
KangDroid <hyunwoo286@gmail.com>
2022-12-19 12:50:06 +0900

    [Chore] Application Infrastructure/AzureSQL: Use ClusterIP instead of LoadBalancer

--- applications/modules/infrastructure/azuresql.tf
+++ applications/modules/infrastructure/azuresql.tf
@@ -2,4 +2,9 @@ resource "helm_release" "sqlserver" {
   name      = "sqlserver"
   chart     = "${path.module}/chart"
   namespace = kubernetes_namespace_v1.name.metadata.0.name
+
+  set {
+    name  = "service.type"
+    value = "ClusterIP"
+  }
 }
