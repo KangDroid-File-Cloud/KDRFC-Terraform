f1b210470203c11e1096daf32c1913188a6d9a49
KangDroid <hyunwoo286@gmail.com>
2023-01-15 17:32:56 +0900

    [Improvements] Specify Community Operator version to 0.7.5

--- kubernetes/mongodb_operator.tf
+++ kubernetes/mongodb_operator.tf
@@ -1,6 +1,7 @@
 resource "helm_release" "mongodb_operator" {
   repository = "https://mongodb.github.io/helm-charts"
   chart      = "community-operator"
+  version    = "0.7.5"
   namespace  = kubernetes_namespace_v1.infrastructure.metadata.0.name
   name       = "mongodb-community-operator"
 
