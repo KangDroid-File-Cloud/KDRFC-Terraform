57d0d2fa64e50b97a991ec895d9e73de994692f6
KangDroid <hyunwoo286@gmail.com>
2023-02-26 13:28:58 +0900

    [Feature] Kubernetes MongoDB Operator: Move to personal MongoDB Operator, supporting aarch64

--- kubernetes/mongodb_operator.tf
+++ kubernetes/mongodb_operator.tf
@@ -1,7 +1,7 @@
 resource "helm_release" "mongodb_operator" {
   repository = "https://mongodb.github.io/helm-charts"
   chart      = "community-operator"
-  version    = "0.7.6"
+  version    = "0.7.8"
   namespace  = kubernetes_namespace_v1.infrastructure.metadata.0.name
   name       = "mongodb-community-operator"
 
@@ -11,40 +11,42 @@ resource "helm_release" "mongodb_operator" {
   }
 
   set {
-    name  = "registry.operator"
-    value = "mohsinonxrm"
+    name  = "operator.version"
+    value = "v0.7.8"
   }
 
   set {
-    name  = "registry.versionUpgradeHook"
-    value = "mohsinonxrm"
-  }
-
-  set {
-    name  = "registry.readinessProbe"
-    value = "mohsinonxrm"
-  }
-
-  set {
-    name  = "registry.agent"
-    value = "mohsinonxrm"
+    name  = "agent.version"
+    value = "12.0.15.7646-1"
   }
 
   set {
     name  = "versionUpgradeHook.version"
-    value = "1.0.5"
+    value = "1.0.6"
   }
 
   set {
-    name  = "readinessProbe.name"
-    value = "mongodb-kubernetes-readiness"
-  }
-  set {
     name  = "readinessProbe.version"
-    value = "1.0.11"
+    value = "1.0.12"
   }
+
   set {
-    name  = "agent.version"
-    value = "12.0.10.7591-1"
+    name  = "registry.agent"
+    value = "kangdroid"
+  }
+
+  set {
+    name  = "registry.versionUpgradeHook"
+    value = "kangdroid"
+  }
+
+  set {
+    name  = "registry.readinessProbe"
+    value = "kangdroid"
+  }
+
+  set {
+    name  = "registry.operator"
+    value = "kangdroid"
   }
 }
